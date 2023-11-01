0700514b403af57aaac7f477f8c0fa74e91235ab
Joergen Ibsen <jibz-git@stdip.com>
2023-11-01 09:24:03 +0100

    Add SPDX license identifiers

--- scv.h
+++ scv.h
@@ -16,6 +16,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef SCV_H_INCLUDED
